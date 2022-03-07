6edb52152da1720244344cdbfe243c1ef7dd6dbc
Rafael Ferreira <rafaelcardoso990@gmail.com>
2022-03-07 19:26:44 -0300

    POC

---
+++ script.js
@@ -0,0 +1,66 @@
+// c    onst texto = document.getElementById('container')
+
+
+// console.log(texto.innerHTML)
+// console.log(texto.innerText)
+// console.log(texto.textContent)
+
+
+
+
+
+// obtendo objetos por seletores diferentes
+
+
+
+// const div = document.querySelectorAll('teste')
+
+// console.log(div)
+
+// const div1 = document.getElementById('#teste')
+
+// console.log(div1)
+
+// const div2 = document.getElementsByClassName('classname')
+
+// console.log(div2)
+
+// const div3 = document.querySelector('teste')
+
+// console.log(div3)
+
+
+
+
+
+
+
+
+
+// Array, nodelist e como tranformar
+ 
+
+// const divarr = Array.from(div)
+
+// const divarr = [...div]
+
+// const divarr = Array.prototype.slice.call(div) // melhor metodo
+
+// console.log(divarr)
+
+
+
+
+
+
+
+// como criar elementos no javascript
+
+// const novoElemento = document.createElement('div')
+
+// console.log(novoElemento)
+
+// // const ola = document.createTextNode('Olá mundo')
+// novoElemento.appendChild(ola)
+
+// console.log(ola)
